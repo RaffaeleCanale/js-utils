d3306d0b5ad1613e2bbc11d6790f01537de584cc
Canale <raffaelecanale@gmail.com>
2018-03-10 16:10:30 +0100

    Add chain

---
+++ chain.js
@@ -0,0 +1,61 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _logger = require('./logger');
+
+var _logger2 = _interopRequireDefault(_logger);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var _class = function () {
+    function _class(tasks) {
+        var _this = this;
+
+        var logger = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : _logger2.default;
+
+        _classCallCheck(this, _class);
+
+        _.forEach(tasks, function (task, name) {
+            _this[name] = _this._append.bind(_this, task);
+        });
+
+        this.tasks = [];
+        this.logger = logger;
+    }
+
+    _createClass(_class, [{
+        key: '_append',
+        value: function _append(task) {
+            var _this2 = this;
+
+            for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+                args[_key - 1] = arguments[_key];
+            }
+
+            this.tasks.push(function () {
+                return task.apply(_this2, args);
+            });
+            return this;
+        }
+    }, {
+        key: 'execute',
+        value: function execute() {
+            var promise = this.tasks[0]();
+            this.tasks.slice(1).forEach(function (t) {
+                promise = promise.then(t);
+            });
+            return promise.catch(this.logger.error);
+        }
+    }]);
+
+    return _class;
+}();
+
+exports.default = _class;
---
+++ src/chain.js
@@ -0,0 +1,26 @@
+import DefaultLogger from './logger';
+
+export default class {
+
+    constructor(tasks, logger = DefaultLogger) {
+        _.forEach(tasks, (task, name) => {
+            this[name] = this._append.bind(this, task);
+        });
+
+        this.tasks = [];
+        this.logger = logger;
+    }
+
+    _append(task, ...args) {
+        this.tasks.push(() => task.apply(this, args));
+        return this;
+    }
+
+    execute() {
+        let promise = this.tasks[0]();
+        this.tasks.slice(1).forEach((t) => {
+            promise = promise.then(t);
+        });
+        return promise.catch(this.logger.error);
+    }
+}
