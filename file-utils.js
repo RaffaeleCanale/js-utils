b1a481efdb561873596473c1ccba1538f6b51ab8
RaffaeleCanale <raffaelecanale@gmail.com>
2018-03-12 19:58:45 +0100

    Update file-utils.js

--- file-utils.js
+++ file-utils.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.readJsonFile = readJsonFile;
+exports.readJson = readJson;
 exports.writeJson = writeJson;
 
 var _fs = require('fs');
@@ -12,7 +12,7 @@ var _fs2 = _interopRequireDefault(_fs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function readJsonFile(file) {
+function readJson(file) {
     return new Promise(function (resolve, reject) {
         _fs2.default.readFile(file, 'utf8', function (err, data) {
             if (err) return reject(err);
