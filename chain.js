29552d88931eb2163ac5a39a38413ede2a0e5dff
Raffaele Canale <raffaelecanale@gmail.com>
2018-04-29 09:56:59 +0200

    Bugfix: missing lodash import

--- chain.js
+++ chain.js
@@ -10,6 +10,10 @@ var _logger = require('./logger');
 
 var _logger2 = _interopRequireDefault(_logger);
 
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -22,7 +26,7 @@ var _class = function () {
 
         _classCallCheck(this, _class);
 
-        _.forEach(tasks, function (task, name) {
+        _lodash2.default.forEach(tasks, function (task, name) {
             _this[name] = _this._append.bind(_this, task);
         });
 
--- src/chain.js
+++ src/chain.js
@@ -1,4 +1,5 @@
 import DefaultLogger from './logger';
+import _ from 'lodash';
 
 export default class {
 
